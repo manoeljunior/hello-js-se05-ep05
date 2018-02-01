72280ef99ed3a2d0863948c6c1fae240beb63454
Manoel Jr <manoeljnr@gmail.com>
2018-02-01 20:41:51 -0300

    Create /listcontato and /addcontato route

---
+++ index.js
@@ -0,0 +1,31 @@
+const cfg = require('./knexfile')
+const knex = require('knex')(cfg.development)
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(morgan('dev'))
+
+app.get('/listcontatos', (req, res) => {
+  knex('contato').select().then(contatos => {
+    res.send(JSON.stringify(contatos))
+  }).catch(err => {
+    res.status(500).send(err)
+    console.log(err)
+  })
+})
+
+app.get('/addcontato', (req, res) => {
+  knex('contato').insert(req.query, 'idcontato').then(contato => {
+    res.send(contato)
+  }).catch(err => {
+    res.status(500).send(err)
+    console.log(err)
+  })
+})
+
+knex.migrate.latest().then(() => {
+  app.listen(3000, () => {
+    console.log('Server is running...')
+  })
+})
