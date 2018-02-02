c234a405c3c2089ccc910a0421424d324bdec0f9
Manoel Jr <manoeljnr@gmail.com>
2018-02-01 21:33:32 -0300

    body-parse and css

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const cfg = require('./knexfile')
 const knex = require('knex')(cfg.development)
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded())
 
 app.get('/listcontatos', (req, res) => {
   knex('contato').select().then(contatos => {
@@ -16,8 +18,8 @@ app.get('/listcontatos', (req, res) => {
   })
 })
 
-app.get('/addcontato', (req, res) => {
-  knex('contato').insert(req.query, 'idcontato').then(contato => {
+app.post('/addcontato', (req, res) => {
+  knex('contato').insert(req.body, 'idcontato').then(contato => {
     res.send(contato)
   }).catch(err => {
     res.status(500).send(err)
